b2aeee9bb5f193ac76d7dedb04777c263153b19f
Rahim Rady <rahimrady@gmail.com>
2025-08-27 13:35:04 +0200

    Update Google Tag Manager integration in layout.tsx
    - Replace existing Google Tag Manager script with gtag.js for improved analytics tracking.
    - Remove unnecessary noscript tag and streamline the script for better performance and clarity.
    - Ensure proper configuration for Google Analytics with the new tracking ID.

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,27 +81,21 @@ export default function RootLayout({
         <link rel="apple-touch-icon" sizes="180x180" href="/Favicon.png" />
         <meta name="msapplication-TileImage" content="/Favicon.png" />
         
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        {/* Google tag (gtag.js) */}
+        <Script 
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZWEB67BZW9" 
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TQ53CMHR');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZWEB67BZW9');
           `}
         </Script>
       </head>
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TQ53CMHR"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Providers>
           {children}
         </Providers>
